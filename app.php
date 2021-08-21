28b1619f61b194581ef65bf301361eb9b51c32b5
Samet Utku Olgun <smtkuo@gmail.com>
2021-08-21 07:27:33 +0300

    Comment lines added

--- app.php
+++ app.php
@@ -3,12 +3,16 @@
 $age = 25;
 
 if($age>=0 && $age<=14){
+    // 0-14 yaş aralığında ise ekrana "Çocuk" yazdırır
     echo "Çocuk";
 }elseif($age>=15 && $age<=24){
+    // 15-24 yaş aralığında ise ekrana "Genç" yazdırır
     echo "Genç";
 }elseif($age>=25 && $age<=64){
+    // 25-64 yaş aralığında ise ekrana "Yetişkin" yazdırır
     echo "Yetişkin";
 }elseif($age>64){
+     // 64 yaşından büyük ise ekrana "Yaşlı" yazdırır
     echo "Yaşlı";
 }
 
